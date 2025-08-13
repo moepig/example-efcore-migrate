14b95af84d0d56eeb7b7e4f17af6a5db04e1814a
moepig <fujita.riki@gmail.com>
2025-08-14 00:27:22 +0900

    Home 関連は /Home/Error 以外消す

--- ExampleEFCoreMigrate/Controllers/HomeController.cs
+++ ExampleEFCoreMigrate/Controllers/HomeController.cs
@@ -13,16 +13,6 @@ public class HomeController : Controller
         _logger = logger;
     }
 
-    public IActionResult Index()
-    {
-        return View();
-    }
-
-    public IActionResult Privacy()
-    {
-        return View();
-    }
-
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
